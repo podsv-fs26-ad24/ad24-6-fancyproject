e96f2de7ddd6d07b600d0c1a737e21ebb0c84a12
muellj16 <muellj16@students.zhaw.ch>
2026-04-12 14:28:07 +0200

    add initial dashboard

---
+++ src/dashboard.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import streamlit as st
+
+conflicts = pd.read_parquet("./data/clean/conflicts.parquet")
+trade = pd.read_parquet("./data/clean/trade.parquet")
+alliances = pd.read_parquet("./data/clean/alliances.parquet")
+
+
+st.title("Geopolitical Conflict & Trade Explorer")
+
+st.write("Hello! This is my dashboard.")
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric("Number of Conflicts", f"{len(conflicts["Conflict_ID"].unique()):,}")
+with col2:
+    year_min, year_max = conflicts["Start_Year"].min(), conflicts["End_Year"].max()
+    st.metric("Year Range", f"{year_min} - {year_max}")
+with col3:
+    st.metric("Number of Countries involved in conflicts", f"{len(conflicts["Statecode_A"].unique()):,}")
+
+st.dataframe(conflicts)
